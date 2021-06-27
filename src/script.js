24f594f9abb2c1114ad9cd88cd0dd5c947fd85e9
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-06-27 13:15:47 +0200

    Added styled modal with hero data, localStorage

--- src/script.js
+++ src/script.js
@@ -1,33 +1,59 @@
-let page = 1
-getHeroesList(page)
+let page;
+ 
+function storePage(page) {
+  localStorage.setItem('page', page)
+}
+
+if (!localStorage.getItem('page')) {
+  page = 1
+  storePage(page)
+  getHeroesList(page) 
+} else {
+  page = localStorage.getItem('page')
+  getHeroesList(page)
+}
 
 const listContainer = document.getElementById('heroes-list')
 const previousBtn = document.getElementById('previous')
 const nextBtn = document.getElementById('next')
 const paginationBtns = document.getElementById('pagination-btns')
+const modal = document.querySelector('.modal')
+const description = document.querySelector('.description')
 
 paginationBtns.addEventListener('click', setPage)
 
+function toggleModal() {
+  modal.classList.toggle('invisible')
+}
+
+function showHeroInfo(hero) {
+  toggleModal();
+  description.innerHTML = `<tr><th scope="row" colspan="2" class='hero-name'>${hero.name}<button onclick='toggleModal()' id='closeBtn'>&#10006</button></th></tr>
+                          <tr><th scope="row" class='hero-data'>Gender</th><td>${hero.gender}</td></tr>
+                          <tr><th scope="row" class='hero-data'>Birth year</th><td>${hero.birth_year}</td></tr>
+                          <tr><th scope="row" class='hero-data'>Eye color</th><td>${hero.eye_color}</td></tr>
+                          <tr><th scope="row" class='hero-data'>Height</th><td>${hero.height}</td></tr>
+                          <tr><th scope="row" class='hero-data'>Mass</th><td>${hero.mass}</td></tr>
+                          <tr><th scope="row" class='hero-data'>Skin color</th><td>${hero.skin_color}</td></tr>`
+}
+
+function toggleImgVisibility(e) {
+  let target = e && e.target
+  if (target && e.type === 'mouseenter') target.childNodes[1].style.visibility = 'visible';
+  if (target && e.type === 'mouseleave') target.childNodes[1].style.visibility = 'hidden';
+}
+
 function setPage(e) {
   let target = e && e.target
-  if (target.getAttribute('id') === 'next' && page < 10) {
+  if (target.getAttribute('id') === 'next' && page < 9) {
     page += 1
     previousBtn.removeAttribute('disabled', 'disabled')
   } else if (target.getAttribute('id') === 'previous' && page > 1) {
     page -= 1
     nextBtn.removeAttribute('disabled', 'disabled')
   }
+  storePage(page)
   getHeroesList(page)
-}
-
-function showHeroInfo(hero) {
-  console.log(hero)
-}
-
-function toggleVisibility(e) {
-  let target = e && e.target
-  if (target && e.type === 'mouseenter') target.childNodes[1].style.visibility = 'visible';
-  if (target && e.type === 'mouseleave') target.childNodes[1].style.visibility = 'hidden';
 }
 
 function displayHeroesList(response) {
@@ -52,8 +78,8 @@ function displayHeroesList(response) {
 
     listContainer.appendChild(liElement)
 
-    liElement.addEventListener('mouseenter', toggleVisibility)
-    liElement.addEventListener('mouseleave', toggleVisibility)
+    liElement.addEventListener('mouseenter', toggleImgVisibility)
+    liElement.addEventListener('mouseleave', toggleImgVisibility)
     liElement.addEventListener('click', showHeroInfo.bind(this, hero))
   })
 }
